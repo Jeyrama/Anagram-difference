df94e936d2aa911e8dbf314747156a01d93a161a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-19 08:58:02 -0700

    Update Anagram-difference.js

--- Anagram-difference.js
+++ Anagram-difference.js
@@ -15,3 +15,21 @@ Hints:
 
 
 // Solution
+
+function anagramDifference(w1,w2){
+  const stack = {};
+  
+  for (let c1 of w1) {
+    stack[c1] > 0 ? stack[c1]++ : stack[c1] = 1;
+  }
+  
+  let counter = 0;
+  
+  for (let c2 of w2) {
+    stack[c2] > 0 ? stack[c2]-- : counter++;
+  }
+  
+  const result = counter + Object.values(stack).reduce((sum, curr) => sum + curr, 0);
+  
+  return result;
+}
